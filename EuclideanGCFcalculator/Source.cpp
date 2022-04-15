6a71883cd887263ee04fa30057cb4b8887eb38b6
Joshua Bolton <jabolton33@gmail.com>
2022-04-15 10:10:10 -0700

    Added negative remainders

--- EuclideanGCFcalculator/Source.cpp
+++ EuclideanGCFcalculator/Source.cpp
@@ -2,17 +2,26 @@
 
 using namespace std;
 
-int remainder(int divedend, int divisor) {
+int pos_remainder(int divedend, int divisor) {
 	for (int quantity{0}; divedend >= divisor; ++quantity) {
-		divedend += -divisor;
+		divedend += -divisor; // subtracting the divisor
 	}
-	return divedend;
+	return divedend; // Remainder willl only every be positive
+}
+
+int neg_remainder(int divedend, int divisor) {
+	for (int quantity{ 0 }; divedend >= divisor; ++quantity) {
+		divedend += -divisor; // subtracting the divisor
+	}
+	// We should return the absolute value of the smaller term remainder (+ or -). They both contain the GCF.
+	int next_divedend = std::abs(divedend - divisor);
+	return (next_divedend < divedend) ? next_divedend : divedend; // This enables negative remainders
 }
 
 int GCF(int num1, int num2) {
 	if (num1 < num2) std::swap(num1, num2); // Make sure num1 is the bigger one
 	cout << "Num1: " << num1 << " Num2: " << num2 << endl;
-	int r = remainder(num1, num2);
+	int r = neg_remainder(num1, num2);
 	if (r == 0) { cout << "One is a factor of the other" << endl;
 	return num2; // the smaller one will always be the factor of the bigger one
 	}
